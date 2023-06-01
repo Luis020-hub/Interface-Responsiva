381a5d67b579e8b3cf97bc86865343db5b8ac46d
Luis020-hub <guhmonteiro10@hotmail.com>
2023-05-31 22:08:03 -0300

    31/05/2023
    - Criando a logica para a responsividade, alterando as telas conforme o tamanho do dispositivo.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:responsive/pages/HomePage.dart';
+import 'package:responsive/pages/home/HomePage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
---
+++ lib/pages/home/HomePage.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+import 'package:responsive/pages/home/widgets/mobile_app_bar.dart';
+import 'package:responsive/pages/home/widgets/web_app.bar.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        return Scaffold(
+          appBar: constraints.minWidth < 800
+              ? const PreferredSize(
+                  preferredSize: Size(double.infinity, 56),
+                  child: MobileAppBar(),
+                )
+              : const PreferredSize(
+                  preferredSize: Size(double.infinity, 72),
+                  child: WebAppBar(),
+                ),
+          drawer: const Drawer(),
+        );
+      },
+    );
+  }
+}
